8156989db6c8d073024bf9e395296b5bba89fbaa
ramshaa-ayub <ramshaa.ayub@gmail.com>
2026-02-14 05:42:00 +0500

    Fix import casing for Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./ScrollToTop";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
